bf9013a2c3c8e5a19ca2e077b453fb66ddb587e9
yeqing <heopro@163.com>
2018-10-04 12:52:09 +0800

    7.28

---
+++ 7.28/main.js
@@ -0,0 +1,21 @@
+var __main = function() {
+    var images = {
+        bullet: 'img/bullet.png',
+        cloud: 'img/cloud.png',
+        player: 'img/player.png',
+        sky: 'img/sky.png',
+        enemy0: 'img/enemy0.png',
+        enemy1: 'img/enemy1.png',
+        enemy2: 'img/enemy2.png',
+        fire: 'img/fire.png',
+    }
+    var game = GuaGame.instance(30, images, function(g){
+        // var s = Scene.new(g)
+        var s = SceneTitle.new(g)
+        g.runWithScene(s)
+    })
+
+    enableDebugMode(game)
+}
+
+__main()
