48e0b70f1f24c74b94eeea4c8f54cca4299f0d90
yeqing <heopro@163.com>
2018-10-17 10:37:12 +0800

    图片获取

---
+++ 8.11/config.js
@@ -0,0 +1,10 @@
+const config = {
+    color: {
+        mario: ['#FFFFFF', '#E05000', '#FFA000', '#888800'],
+        groud: ['#FFFFFF', '#FFFFFF', '#C07000', '#000000'],
+        question_mark: ['#FFFFFF', '#C07000', '#C07000', '#000000'],
+        brick: ['#FFFFFF', '#000000', '#FFFFFF', '#C07000'],
+        cloud: ['#FFFFFF', '#FFFFFF', '#60A0FF', '#000000'],
+        grass: ['#FFFFFF', '#98E800', '#48A810', '#000000'],
+    },
+}
---
+++ 8.11/control.js
@@ -0,0 +1,79 @@
+const control = {
+    width: {
+        _comment: '拼图版宽度',
+        value: 96,
+    },
+    height: {
+        _comment: '拼图版高度',
+        value: 96,
+    },
+}
+
+var es = sel => document.querySelectorAll(sel)
+
+var insertDebugBox = function() {
+    var debugBox = document.querySelector('.gua-control')
+    var items = Object.keys(control)
+    // log(setting)
+    for (var i = 0; i < items.length; i++) {
+        var s = items[i]
+        var t = `
+        <div>
+            <label>
+                <input class="gua-auto-slider" type="range"
+                max="1000"
+                step="24"
+                value="${control[s].value}"
+                data-value="control.${s}"
+                >
+                ${control[s]._comment}：<span class="gua-label">${control[s].value}</span>
+            </label>
+        </div>
+        `
+        debugBox.insertAdjacentHTML('beforeend', t)
+    }
+}
+
+var updateCanvasSize = () => {
+    let canvas = e('#id-canvas-tile')
+    canvas.width = control.width.value
+    canvas.height = control.height.value
+    // setInterval(function() {
+    //     let canvas = e('#id-canvas-tile')
+    //     if (canvas.width != control.width.value) {
+    //         canvas.width = control.width.value
+    //     } else if (canvas.height != control.height.value) {
+    //         canvas.height = control.height.value
+    //     }
+    // }, 200)
+}
+
+var bindAll = function(sel, eventName, callback) {
+    var l = es(sel)
+    for (var i = 0; i < l.length; i++) {
+        var input = l[i]
+        input.addEventListener(eventName, function(event) {
+            callback(event)
+        })
+    }
+}
+
+var bindChangeEvents = function() {
+    bindAll('.gua-auto-slider', 'input', function(event) {
+        var target = event.target
+        var bindVar = target.dataset.value
+        var v = target.value
+        eval(bindVar + '.value =' + v)
+        var label = target.closest('label').querySelector('.gua-label')
+        label.innerText = v
+        updateCanvasSize()
+    })
+}
+
+var enableDebugMode = function(game) {
+    insertDebugBox()
+    bindChangeEvents()
+    // updateCanvasSize()
+}
+
+enableDebugMode()
